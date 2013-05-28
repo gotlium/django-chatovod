f26ce296a60ba75c0adc51e2a15a9a63ed29bc9b
gotlium <death-metall@mail.ru>
2013-05-28 18:17:19 +0500

    fixes for social_auth

--- chatovod/templatetags/chatovod.py
+++ chatovod/templatetags/chatovod.py
@@ -16,13 +16,13 @@ def chatovod(context):
         return
     request = context['request']
     if request.user.is_authenticated():
+        auth_hash = u'%s%s%s' % (request.user.username, request.user.email,
+                                 settings.CHAT_API_KEY)
         query = {
-            'anick': request.user.username,
+            'anick': request.user.username.encode('utf8'),
             'aemail': request.user.email,
             'amode': 'django',
-            'akey': md5(
-                '%s%s%s' % (request.user.username, request.user.email,
-                            settings.CHAT_API_KEY)).hexdigest()
+            'akey': md5(auth_hash.encode('utf8')).hexdigest()
         }
         query.update(settings.CHAT_QUERY)
     else:
